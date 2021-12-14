f7f07604f5200975bd80d86cf86aa92eaff3ca67
monoteist <i.ezerbaev97@gmail.com>
2021-12-14 11:46:50 +0300

    test data added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh 
 
 
-python manage.py makemigrations --noinput 
-python manage.py migrate --noinput 
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py loaddata db.json
 
 exec "$@" 
